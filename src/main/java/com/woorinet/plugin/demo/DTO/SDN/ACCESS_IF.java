8029a9669a1ecfc46f23f42e528e9442924a804e
jjm2317 <wlaks2317@gmail.com>
2021-02-16 14:27:58 +0900

    [ADD] All SDN Classes to dir 'SDN'

---
+++ src/main/java/com/woorinet/plugin/demo/DTO/SDN/ACCESS_IF.java
@@ -0,0 +1,50 @@
+package com.woorinet.plugin.demo.DTO.SDN;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ACCESS_IF {
+    int ems_id;
+    String accessif_id;
+    String accessif_name;
+    String ne_id;
+    String connector_id;
+    String accessif_type;
+    String accessif_status;
+    String service_ref;
+    String node_connector_ref;
+
+    public ACCESS_IF() {
+
+    }
+
+    public ACCESS_IF(int ems_id, String accessif_id, String accessif_name, String ne_id, String connector_id, String accessif_type, String accessif_status, String service_ref, String node_connector_ref) {
+        this.ems_id = ems_id;
+        this.accessif_id = accessif_id;
+        this.accessif_name = accessif_name;
+        this.ne_id = ne_id;
+        this.connector_id = connector_id;
+        this.accessif_type = accessif_type;
+        this.accessif_status = accessif_status;
+        this.service_ref = service_ref;
+        this.node_connector_ref = node_connector_ref;
+    }
+
+    @Override
+    public String toString() {
+        return "ACCESS_IF{" +
+                "ems_id=" + ems_id +
+                ", accessif_id='" + accessif_id + '\'' +
+                ", accessif_name='" + accessif_name + '\'' +
+                ", ne_id='" + ne_id + '\'' +
+                ", connector_id='" + connector_id + '\'' +
+                ", accessif_type='" + accessif_type + '\'' +
+                ", accessif_status='" + accessif_status + '\'' +
+                ", service_ref='" + service_ref + '\'' +
+                ", node_connector_ref='" + node_connector_ref + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/woorinet/plugin/demo/DTO/SDN/CONSTRAINT.java
@@ -0,0 +1,44 @@
+package com.woorinet.plugin.demo.DTO.SDN;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CONSTRAINT {
+    int ems_id;
+    String service_id;
+    String const_id;
+    String const_type;
+    String const_name;
+    String const_value;
+    String const_operator;
+
+    public CONSTRAINT() {
+
+    }
+
+    public CONSTRAINT(int ems_id, String service_id, String const_id, String const_type, String const_name, String const_value, String const_operator) {
+        this.ems_id = ems_id;
+        this.service_id = service_id;
+        this.const_id = const_id;
+        this.const_type = const_type;
+        this.const_name = const_name;
+        this.const_value = const_value;
+        this.const_operator = const_operator;
+    }
+
+    @Override
+    public String toString() {
+        return "CONSTRAINT{" +
+                "ems_id=" + ems_id +
+                ", service_id='" + service_id + '\'' +
+                ", const_id='" + const_id + '\'' +
+                ", const_type='" + const_type + '\'' +
+                ", const_name='" + const_name + '\'' +
+                ", const_value='" + const_value + '\'' +
+                ", const_operator='" + const_operator + '\'' +
+                '}';
+    }
+}
