7a6b2a64855851b8e5b7fad5d677ccdf42f9d996
sinkyoungdeok <tlsrudejr123@nate.com>
2021-03-01 17:18:47 +0900

    [FIX] slot refactorying complete

--- src/main/java/com/woorinet/plugin/demo/Mapper/TL1Mapper.java
+++ src/main/java/com/woorinet/plugin/demo/Mapper/TL1Mapper.java
@@ -40,7 +40,7 @@ public interface TL1Mapper {
 
     List<NODE> selectNode() throws Exception;
     List<Tl1SystemInfo> selectSystemInfo() throws Exception;
-    List<SLOT> selectSlot() throws Exception;
+    List<Tl1Slot> selectSlot() throws Exception;
     List<ETH_PORT> selectEthPort() throws Exception;
     List<NODECONNECTOR> selectNodeConnector() throws Exception;
     List<CES_NODE_CONNECTOR> selectCesNodeConnector() throws Exception;
@@ -67,7 +67,7 @@ public interface TL1Mapper {
 
     void insertNode(NODE node) throws Exception;
     void insertSystemInfo(Tl1SystemInfo systemInfo) throws Exception;
-    void insertSlot(SLOT slot) throws Exception;
+    void insertSlot(Tl1Slot tl1Slot) throws Exception;
     void insertEthPort(ETH_PORT ethPort) throws Exception;
     void insertNodeConnector(NODECONNECTOR nodeConnector) throws Exception;
     void insertCesNodeConnector(CES_NODE_CONNECTOR cesNodeConnector) throws Exception;
