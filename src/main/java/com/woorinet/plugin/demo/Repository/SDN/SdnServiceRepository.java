a1f3bf2ea8a932c93b09b9ae603bf02c659a32d5
sinkyoungdeok <tlsrudejr123@nate.com>
2021-03-03 12:08:28 +0900

    [FIX] (service) bug

--- src/main/java/com/woorinet/plugin/demo/Repository/SDN/SdnServiceRepository.java
+++ src/main/java/com/woorinet/plugin/demo/Repository/SDN/SdnServiceRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface SdnServiceRepository extends JpaRepository<SdnService, String> {
-    @Query("select s from SERVICE s where s.service_name = :service_name")
+    @Query("select s from sdn_service s where s.service_name = :service_name")
     SdnService findByService_name (@Param("service_name") String service_name);
 }
