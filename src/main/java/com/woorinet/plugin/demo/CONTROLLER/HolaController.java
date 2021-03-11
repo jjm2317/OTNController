b5ea561048feb926740ec937c8244cb1d48e201c
jjm2317 <wlaks2317@gmail.com>
2021-03-11 14:45:40 +0900

    [ADD] Hola swagger

--- src/main/java/com/woorinet/plugin/demo/CONTROLLER/HolaController.java
+++ src/main/java/com/woorinet/plugin/demo/CONTROLLER/HolaController.java
@@ -4,9 +4,12 @@ import com.woorinet.plugin.demo.DTO.SDN.*;
 import com.woorinet.plugin.demo.Manager.HOLAManager;
 import com.woorinet.plugin.demo.Repository.HOLA.*;
 import com.woorinet.plugin.demo.Repository.SDN.*;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -90,5 +93,53 @@ public class HolaController {
         }
 
         return "sync hola";
+    }
+
+    @ApiOperation(value = "get Line Number Sheet", notes = "전체 선번장 조회")
+    @GetMapping(value = "/lineNumSheet")
+    @ResponseBody
+    public ResponseEntity selectLineNumSheetList() {
+        if(holaLineNumSheetRepository.findAll() == null) return ResponseEntity.notFound().build();
+        return ResponseEntity.ok(holaLineNumSheetRepository.findAll());
+    }
+
+    @ApiOperation(value = "get link mng", notes = "전체 링크관리 조회")
+    @GetMapping(value = "/linkMng")
+    @ResponseBody
+    public ResponseEntity selectLinkMngList() {
+        if(holaLinkMngRepository.findAll() == null) return ResponseEntity.notFound().build();
+        return ResponseEntity.ok(holaLinkMngRepository.findAll());
+    }
+
+    @ApiOperation(value = "get trunk usage", notes = "전체 trunk 사용률 조회")
+    @GetMapping(value = "/trunkUsage")
+    @ResponseBody
+    public ResponseEntity selectTrunkUsageList() {
+        if(holaTrunkUsageRepository.findAll() == null) return ResponseEntity.notFound().build();
+        return ResponseEntity.ok(holaTrunkUsageRepository.findAll());
+    }
+
+    @ApiOperation(value = "get inventory detail", notes = "전체 상새 inventory 현황 조회")
+    @GetMapping(value = "/inventoryDetail")
+    @ResponseBody
+    public ResponseEntity selectInventoryDetail() {
+        if(holaInventroyDetailRepository.findAll() == null) return ResponseEntity.notFound().build();
+        return ResponseEntity.ok(holaInventroyDetailRepository.findAll());
+    }
+
+    @ApiOperation(value = "get otn node usage", notes = "전체 otn node 사용현황 조회")
+    @GetMapping(value = "/otnNodeUsage")
+    @ResponseBody
+    public ResponseEntity selectOtnNodeUsage() {
+        if(holaOtnNodeUsageRepository.findAll() == null) return ResponseEntity.notFound().build();
+        return ResponseEntity.ok(holaOtnNodeUsageRepository.findAll());
+    }
+
+    @ApiOperation(value = "get otn material", notes = "전체 otn 물자현황 조회")
+    @GetMapping(value = "/otnMaterial")
+    @ResponseBody
+    public ResponseEntity selectOtnMaterial() {
+        if(holaOtnMaterialRepository.findAll() == null) return ResponseEntity.notFound().build();
+        return ResponseEntity.ok(holaOtnMaterialRepository.findAll());
     }
 }
--- src/main/java/com/woorinet/plugin/demo/DTO/HOLA/HolaTrunkUsage.java
+++ src/main/java/com/woorinet/plugin/demo/DTO/HOLA/HolaTrunkUsage.java
@@ -19,4 +19,48 @@ public class HolaTrunkUsage {
     @Id
     @GeneratedValue
     int HOLA_SDN_TRUNK_USAGE;
+    String VENDOR;
+    String LINK;
+    String ADMIN_WEIGHT;
+    String MAXIMUM_USAGE_RATIO;
+    String MEMORY;
+    String USAGE_RATIO;
+    String DISTANCE;
+    String SRLG;
+    String ROADM_PATH;
+    String REMARKS;
+
+    public HolaTrunkUsage() {
+    }
+
+    public HolaTrunkUsage(String VENDOR, String LINK, String ADMIN_WEIGHT, String MAXIMUM_USAGE_RATIO, String MEMORY, String USAGE_RATIO, String DISTANCE, String SRLG, String ROADM_PATH, String REMARKS) {
+        this.VENDOR = VENDOR;
+        this.LINK = LINK;
+        this.ADMIN_WEIGHT = ADMIN_WEIGHT;
+        this.MAXIMUM_USAGE_RATIO = MAXIMUM_USAGE_RATIO;
+        this.MEMORY = MEMORY;
+        this.USAGE_RATIO = USAGE_RATIO;
+        this.DISTANCE = DISTANCE;
+        this.SRLG = SRLG;
+        this.ROADM_PATH = ROADM_PATH;
+        this.REMARKS = REMARKS;
+    }
+
+    @Override
+    public String toString() {
+        return "HolaTrunkUsage{" +
+                "VENDOR='" + VENDOR + '\'' +
+                ", LINK='" + LINK + '\'' +
+                ", ADMIN_WEIGHT='" + ADMIN_WEIGHT + '\'' +
+                ", MAXIMUM_USAGE_RATIO='" + MAXIMUM_USAGE_RATIO + '\'' +
+                ", MEMORY='" + MEMORY + '\'' +
+                ", USAGE_RATIO='" + USAGE_RATIO + '\'' +
+                ", DISTANCE='" + DISTANCE + '\'' +
+                ", SRLG='" + SRLG + '\'' +
+                ", ROADM_PATH='" + ROADM_PATH + '\'' +
+                ", REMARKS='" + REMARKS + '\'' +
+                '}';
+    }
+
+
 }
