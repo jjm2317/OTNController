b796045000e5fbffab6dbaea55b4687c3289811b
jjm2317 <wlaks2317@gmail.com>
2021-03-11 15:02:31 +0900

    [FIX] get mapping

--- src/main/java/com/woorinet/plugin/demo/CONTROLLER/ViewController.java
+++ src/main/java/com/woorinet/plugin/demo/CONTROLLER/ViewController.java
@@ -1,13 +1,14 @@
 package com.woorinet.plugin.demo.CONTROLLER;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
 public class ViewController {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     String home() {
         return "index";
     }
