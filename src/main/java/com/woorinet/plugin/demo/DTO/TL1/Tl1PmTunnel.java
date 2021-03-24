bf1e332eee59ae0e1cf7a8d7e4b227aac1dca3b7
sinkyoungdeok <tlsrudejr123@nate.com>
2021-03-24 19:54:36 +0900

    [UPDATE] pm_tunnel 필드

--- src/main/java/com/woorinet/plugin/demo/DTO/TL1/Tl1PmTunnel.java
+++ src/main/java/com/woorinet/plugin/demo/DTO/TL1/Tl1PmTunnel.java
@@ -18,84 +18,72 @@ import javax.persistence.Table;
 public class Tl1PmTunnel {
     @Id
     @GeneratedValue
-    int PM_TUNNEL_ID;
-    String TID;
-    String TUNNEL_NAME;
-    String TUNNEL_ID;
-    String LSP_ID;
-    String ING_LSRID;
-    String EGR_LSRID;
-    String TYPE;
-    String DATE;
-    String TIME;
-    String ING_PKT;
-    String ING_BYTE;
-    String ING_RATE;
-    String EGR_PKT;
-    String EGR_BYTE;
-    String EGR_RATE;
-    String LM_COUNTER;
+    int pmTunnelId;
+    String tid;
+    String systemType;
+    String slot;
+    String port;
+    String time;
+    String name;
+    String ingPackets;
+    String ingBytes;
+    String ingRate;
+    String egrPackets;
+    String egrBytes;
+    String egrRate;
+    String date;
 
     public Tl1PmTunnel() {
     }
 
-    public Tl1PmTunnel(String TID, String TUNNEL_NAME, String TUNNEL_ID, String LSP_ID, String ING_LSRID, String EGR_LSRID, String TYPE, String DATE, String TIME, String ING_PKT, String ING_BYTE, String ING_RATE, String EGR_PKT, String EGR_BYTE, String EGR_RATE, String LM_COUNTER) {
-        this.TID = TID;
-        this.TUNNEL_NAME = TUNNEL_NAME;
-        this.TUNNEL_ID = TUNNEL_ID;
-        this.LSP_ID = LSP_ID;
-        this.ING_LSRID = ING_LSRID;
-        this.EGR_LSRID = EGR_LSRID;
-        this.TYPE = TYPE;
-        this.DATE = DATE;
-        this.TIME = TIME;
-        this.ING_PKT = ING_PKT;
-        this.ING_BYTE = ING_BYTE;
-        this.ING_RATE = ING_RATE;
-        this.EGR_PKT = EGR_PKT;
-        this.EGR_BYTE = EGR_BYTE;
-        this.EGR_RATE = EGR_RATE;
-        this.LM_COUNTER = LM_COUNTER;
+    public Tl1PmTunnel(String tid, String systemType, String slot, String port, String time, String name, String ingPackets, String ingBytes, String ingRate, String egrPackets, String egrBytes, String egrRate, String date) {
+        this.tid = tid;
+        this.systemType = systemType;
+        this.slot = slot;
+        this.port = port;
+        this.time = time;
+        this.name = name;
+        this.ingPackets = ingPackets;
+        this.ingBytes = ingBytes;
+        this.ingRate = ingRate;
+        this.egrPackets = egrPackets;
+        this.egrBytes = egrBytes;
+        this.egrRate = egrRate;
+        this.date = date;
     }
 
     public Tl1PmTunnel(String[] fields) {
-        this.TID = fields[0];
-        this.TUNNEL_NAME = fields[1];
-        this.TUNNEL_ID = fields[2];
-        this.LSP_ID = fields[3];
-        this.ING_LSRID = fields[4];
-        this.EGR_LSRID = fields[5];
-        this.TYPE = fields[6];
-        this.DATE = fields[7];
-        this.TIME = fields[8];
-        this.ING_PKT = fields[9];
-        this.ING_BYTE = fields[10];
-        this.ING_RATE = fields[11];
-        this.EGR_PKT = fields[12];
-        this.EGR_BYTE = fields[13];
-        this.EGR_RATE = fields[14];
-        this.LM_COUNTER = fields[15];
+        this.tid = fields[0];
+        this.systemType = fields[1];
+        this.slot = fields[2];
+        this.port = fields[3];
+        this.time = fields[4];
+        this.name = fields[5];
+        this.ingPackets = fields[6];
+        this.ingBytes = fields[7];
+        this.ingRate = fields[8];
+        this.egrPackets = fields[9];
+        this.egrBytes = fields[10];
+        this.egrRate = fields[11];
+        this.date = fields[12];
     }
 
     @Override
     public String toString() {
-        return "PM_TUNNEL{" +
-                "TID='" + TID + '\'' +
-                ", TUNNEL_NAME='" + TUNNEL_NAME + '\'' +
-                ", TUNNEL_ID='" + TUNNEL_ID + '\'' +
-                ", LSP_ID='" + LSP_ID + '\'' +
-                ", ING_LSRID='" + ING_LSRID + '\'' +
-                ", EGR_LSRID='" + EGR_LSRID + '\'' +
-                ", TYPE='" + TYPE + '\'' +
-                ", DATE='" + DATE + '\'' +
-                ", TIME='" + TIME + '\'' +
-                ", ING_PKT=" + ING_PKT +
-                ", ING_BYTE=" + ING_BYTE +
-                ", ING_RATE=" + ING_RATE +
-                ", EGR_PKT=" + EGR_PKT +
-                ", EGR_BYTE=" + EGR_BYTE +
-                ", EGR_RATE=" + EGR_RATE +
-                ", LM_COUNTER='" + LM_COUNTER + '\'' +
+        return "Tl1PmTunnel{" +
+                "tid='" + tid + '\'' +
+                ", systemType='" + systemType + '\'' +
+                ", slot='" + slot + '\'' +
+                ", port='" + port + '\'' +
+                ", time='" + time + '\'' +
+                ", name='" + name + '\'' +
+                ", ingPackets='" + ingPackets + '\'' +
+                ", ingBytes='" + ingBytes + '\'' +
+                ", ingRate='" + ingRate + '\'' +
+                ", egrPackets='" + egrPackets + '\'' +
+                ", egrBytes='" + egrBytes + '\'' +
+                ", egrRate='" + egrRate + '\'' +
+                ", date='" + date + '\'' +
                 '}';
     }
 }
