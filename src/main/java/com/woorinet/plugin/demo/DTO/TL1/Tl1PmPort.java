b73b02ed31e682eb96f6a1d46978578cd75ad0d2
sinkyoungdeok <tlsrudejr123@nate.com>
2021-03-25 14:21:06 +0900

    [UPDATE] pm_port 필드

--- src/main/java/com/woorinet/plugin/demo/DTO/TL1/Tl1PmPort.java
+++ src/main/java/com/woorinet/plugin/demo/DTO/TL1/Tl1PmPort.java
@@ -18,129 +18,181 @@ import javax.persistence.Table;
 public class Tl1PmPort {
     @Id
     @GeneratedValue
-    int PM_PORT_ID;
-    String TID;
-    String AID;
-    String UNIT;
-    String TYPE;
-    String DATE;
-    String TIME;
-    String IN_OCTETS;
-    String IN_OK_PACKETS;
-    String IN_UNICAST_PACKETS;
-    String IN_NON_UNICAST_PACKETS;
-    String IN_DISCARD_PACKETS;
-    String IN_ERROR_PACKETS;
-    String IN_BROADCAST_PACKETS;
-    String IN_MULTICAST_PACKETS;
-    String IN_PACKETS_64_OCTETS;
-    String IN_PACKETS_65_TO_127_OCTETS;
-    String IN_PACKETS_128_TO_255_OCTETS;
-    String IN_PACKETS_256_TO_511_OCTETS;
-    String IN_PACKETS_512_TO_1023_OCTETS;
-    String IN_PACKETS_1024_TO_1518_OCTETS;
-    String IN_PACKETS_1519_TO_MAX_OCTETS;
-    String IN_PAUSE_PACKETS;
-    String IN_ALIGNMENT_ERRORS;
-    String IN_FCS_ERRORS;
-    String IN_SYMBOL_ERRORS;
-    String OUT_OCTETS;
-    String OUT_OK_PACKETS;
-    String OUT_UNICAST_PACKETS;
-    String OUT_NON_UNICAST_PACKETS;
-    String OUT_DISCARD_PACKETS;
-    String OUT_ERROR_PACKETS;
-    String OUT_BROADCAST_PACKETS;
-    String OUT_MULTICAST_PACKETS;
-    String OUT_PAUSE_PACKETS;
-    String IN_RATE;
-    String OUT_RATE;
-    String IN_LOSS_RATIO;
-    String OUT_LOSS_RATIO;
+    int pmPortId;
+    String tid;
+    String systemType;
+    String slot;
+    String port;
+    String time;
+    String inOctets;
+    String inOkPackets;
+    String inUnicastPackets;
+    String inNonUnicastPackets;
+    String inDiscardPackets;
+    String inErrorPackets;
+    String inBroadcastPackets;
+    String inMulticastPackets;
+    String inPackets64Octets;
+    String inPackets65To127Octets;
+    String inPackets128To255Octets;
+    String inPackets256To511Octets;
+    String inPackets512To1023Octets;
+    String inPackets1024To1518Octets;
+    String inPackets1519ToMaxOctets;
+    String inPausePackets;
+    String inAlignmentErrors;
+    String inFcsErrors;
+    String inSymbolErrors;
+    String outOctets;
+    String outOkPackets;
+    String outUnicastPackets;
+    String outNonUnicastPackets;
+    String outDiscardPackets;
+    String outErrorPackets;
+    String outBroadcastPackets;
+    String outMulticastPackets;
+    String outPausePackets;
+    String inRate;
+    String outRate;
+    String inNoPktSec;
+    String outNoPktSec;
+    String inLossRatio;
+    String outLossRatio;
+    String date;
 
     public Tl1PmPort() {
 
     }
 
-    public Tl1PmPort(String []fields) {
-        this.TID = fields[0];
-        this.AID = fields[1];
-        this.UNIT = fields[2];
-        this.TYPE = fields[3];
-        this.DATE = fields[4];
-        this.TIME = fields[5];
-        this.IN_OCTETS = fields[6];
-        this.IN_OK_PACKETS = fields[7];
-        this.IN_UNICAST_PACKETS = fields[8];
-        this.IN_NON_UNICAST_PACKETS = fields[9];
-        this.IN_DISCARD_PACKETS = fields[10];
-        this.IN_ERROR_PACKETS = fields[11];
-        this.IN_BROADCAST_PACKETS = fields[12];
-        this.IN_MULTICAST_PACKETS = fields[13];
-        this.IN_PACKETS_64_OCTETS = fields[14];
-        this.IN_PACKETS_65_TO_127_OCTETS = fields[15];
-        this.IN_PACKETS_128_TO_255_OCTETS = fields[16];
-        this.IN_PACKETS_256_TO_511_OCTETS = fields[17];
-        this.IN_PACKETS_512_TO_1023_OCTETS = fields[18];
-        this.IN_PACKETS_1024_TO_1518_OCTETS = fields[19];
-        this.IN_PACKETS_1519_TO_MAX_OCTETS = fields[20];
-        this.IN_PAUSE_PACKETS = fields[21];
-        this.IN_ALIGNMENT_ERRORS = fields[22];
-        this.IN_FCS_ERRORS = fields[23];
-        this.IN_SYMBOL_ERRORS = fields[24];
-        this.OUT_OCTETS = fields[25];
-        this.OUT_OK_PACKETS = fields[26];
-        this.OUT_UNICAST_PACKETS = fields[27];
-        this.OUT_NON_UNICAST_PACKETS = fields[28];
-        this.OUT_DISCARD_PACKETS = fields[29];
-        this.OUT_ERROR_PACKETS = fields[30];
-        this.OUT_BROADCAST_PACKETS = fields[31];
-        this.OUT_MULTICAST_PACKETS = fields[32];
-        this.OUT_PAUSE_PACKETS = fields[33];
-        this.IN_RATE = fields[34];
-        this.OUT_RATE = fields[35];
-        this.IN_LOSS_RATIO = fields[36];
-        this.OUT_LOSS_RATIO = fields[37];
+    public Tl1PmPort(String tid, String systemType, String slot, String port, String time, String inOctets, String inOkPackets, String inUnicastPackets, String inNonUnicastPackets, String inDiscardPackets, String inErrorPackets, String inBroadcastPackets, String inMulticastPackets, String inPackets64Octets, String inPackets65To127Octets, String inPackets128To255Octets, String inPackets256To511Octets, String inPackets512To1023Octets, String inPackets1024To1518Octets, String inPackets1519ToMaxOctets, String inPausePackets, String inAlignmentErrors, String inFcsErrors, String inSymbolErrors, String outOctets, String outOkPackets, String outUnicastPackets, String outNonUnicastPackets, String outDiscardPackets, String outErrorPackets, String outBroadcastPackets, String outMulticastPackets, String outPausePackets, String inRate, String outRate, String inNoPktSec, String outNoPktSec, String inLossRatio, String outLossRatio, String date) {
+        this.tid = tid;
+        this.systemType = systemType;
+        this.slot = slot;
+        this.port = port;
+        this.time = time;
+        this.inOctets = inOctets;
+        this.inOkPackets = inOkPackets;
+        this.inUnicastPackets = inUnicastPackets;
+        this.inNonUnicastPackets = inNonUnicastPackets;
+        this.inDiscardPackets = inDiscardPackets;
+        this.inErrorPackets = inErrorPackets;
+        this.inBroadcastPackets = inBroadcastPackets;
+        this.inMulticastPackets = inMulticastPackets;
+        this.inPackets64Octets = inPackets64Octets;
+        this.inPackets65To127Octets = inPackets65To127Octets;
+        this.inPackets128To255Octets = inPackets128To255Octets;
+        this.inPackets256To511Octets = inPackets256To511Octets;
+        this.inPackets512To1023Octets = inPackets512To1023Octets;
+        this.inPackets1024To1518Octets = inPackets1024To1518Octets;
+        this.inPackets1519ToMaxOctets = inPackets1519ToMaxOctets;
+        this.inPausePackets = inPausePackets;
+        this.inAlignmentErrors = inAlignmentErrors;
+        this.inFcsErrors = inFcsErrors;
+        this.inSymbolErrors = inSymbolErrors;
+        this.outOctets = outOctets;
+        this.outOkPackets = outOkPackets;
+        this.outUnicastPackets = outUnicastPackets;
+        this.outNonUnicastPackets = outNonUnicastPackets;
+        this.outDiscardPackets = outDiscardPackets;
+        this.outErrorPackets = outErrorPackets;
+        this.outBroadcastPackets = outBroadcastPackets;
+        this.outMulticastPackets = outMulticastPackets;
+        this.outPausePackets = outPausePackets;
+        this.inRate = inRate;
+        this.outRate = outRate;
+        this.inNoPktSec = inNoPktSec;
+        this.outNoPktSec = outNoPktSec;
+        this.inLossRatio = inLossRatio;
+        this.outLossRatio = outLossRatio;
+        this.date = date;
     }
 
-    public Tl1PmPort(String TID, String AID, String UNIT, String TYPE, String DATE, String TIME, String IN_OCTETS, String IN_OK_PACKETS, String IN_UNICAST_PACKETS, String IN_NON_UNICAST_PACKETS, String IN_DISCARD_PACKETS, String IN_ERROR_PACKETS, String IN_BROADCAST_PACKETS, String IN_MULTICAST_PACKETS, String IN_PACKETS_64_OCTETS, String IN_PACKETS_65_TO_127_OCTETS, String IN_PACKETS_128_TO_255_OCTETS, String IN_PACKETS_256_TO_511_OCTETS, String IN_PACKETS_512_TO_1023_OCTETS, String IN_PACKETS_1024_TO_1518_OCTETS, String IN_PACKETS_1519_TO_MAX_OCTETS, String IN_PAUSE_PACKETS, String IN_ALIGNMENT_ERRORS, String IN_FCS_ERRORS, String IN_SYMBOL_ERRORS, String OUT_OCTETS, String OUT_OK_PACKETS, String OUT_UNICAST_PACKETS, String OUT_NON_UNICAST_PACKETS, String OUT_DISCARD_PACKETS, String OUT_ERROR_PACKETS, String OUT_BROADCAST_PACKETS, String OUT_MULTICAST_PACKETS, String OUT_PAUSE_PACKETS, String IN_RATE, String OUT_RATE, String IN_LOSS_RATIO, String OUT_LOSS_RATIO) {
-        this.TID = TID;
-        this.AID = AID;
-        this.UNIT = UNIT;
-        this.TYPE = TYPE;
-        this.DATE = DATE;
-        this.TIME = TIME;
-        this.IN_OCTETS = IN_OCTETS;
-        this.IN_OK_PACKETS = IN_OK_PACKETS;
-        this.IN_UNICAST_PACKETS = IN_UNICAST_PACKETS;
-        this.IN_NON_UNICAST_PACKETS = IN_NON_UNICAST_PACKETS;
-        this.IN_DISCARD_PACKETS = IN_DISCARD_PACKETS;
-        this.IN_ERROR_PACKETS = IN_ERROR_PACKETS;
-        this.IN_BROADCAST_PACKETS = IN_BROADCAST_PACKETS;
-        this.IN_MULTICAST_PACKETS = IN_MULTICAST_PACKETS;
-        this.IN_PACKETS_64_OCTETS = IN_PACKETS_64_OCTETS;
-        this.IN_PACKETS_65_TO_127_OCTETS = IN_PACKETS_65_TO_127_OCTETS;
-        this.IN_PACKETS_128_TO_255_OCTETS = IN_PACKETS_128_TO_255_OCTETS;
-        this.IN_PACKETS_256_TO_511_OCTETS = IN_PACKETS_256_TO_511_OCTETS;
-        this.IN_PACKETS_512_TO_1023_OCTETS = IN_PACKETS_512_TO_1023_OCTETS;
-        this.IN_PACKETS_1024_TO_1518_OCTETS = IN_PACKETS_1024_TO_1518_OCTETS;
-        this.IN_PACKETS_1519_TO_MAX_OCTETS = IN_PACKETS_1519_TO_MAX_OCTETS;
-        this.IN_PAUSE_PACKETS = IN_PAUSE_PACKETS;
-        this.IN_ALIGNMENT_ERRORS = IN_ALIGNMENT_ERRORS;
-        this.IN_FCS_ERRORS = IN_FCS_ERRORS;
-        this.IN_SYMBOL_ERRORS = IN_SYMBOL_ERRORS;
-        this.OUT_OCTETS = OUT_OCTETS;
-        this.OUT_OK_PACKETS = OUT_OK_PACKETS;
-        this.OUT_UNICAST_PACKETS = OUT_UNICAST_PACKETS;
-        this.OUT_NON_UNICAST_PACKETS = OUT_NON_UNICAST_PACKETS;
-        this.OUT_DISCARD_PACKETS = OUT_DISCARD_PACKETS;
-        this.OUT_ERROR_PACKETS = OUT_ERROR_PACKETS;
-        this.OUT_BROADCAST_PACKETS = OUT_BROADCAST_PACKETS;
-        this.OUT_MULTICAST_PACKETS = OUT_MULTICAST_PACKETS;
-        this.OUT_PAUSE_PACKETS = OUT_PAUSE_PACKETS;
-        this.IN_RATE = IN_RATE;
-        this.OUT_RATE = OUT_RATE;
-        this.IN_LOSS_RATIO = IN_LOSS_RATIO;
-        this.OUT_LOSS_RATIO = OUT_LOSS_RATIO;
+    public Tl1PmPort(String []fields) {
+        this.tid = fields[0];
+        this.systemType = fields[1];
+        this.slot = fields[2];
+        this.port = fields[3];
+        this.time = fields[4];
+        this.inOctets = fields[5];
+        this.inOkPackets = fields[6];
+        this.inUnicastPackets = fields[7];
+        this.inNonUnicastPackets = fields[8];
+        this.inDiscardPackets = fields[9];
+        this.inErrorPackets = fields[10];
+        this.inBroadcastPackets = fields[11];
+        this.inMulticastPackets = fields[12];
+        this.inPackets64Octets = fields[13];
+        this.inPackets65To127Octets = fields[14];
+        this.inPackets128To255Octets = fields[15];
+        this.inPackets256To511Octets = fields[16];
+        this.inPackets512To1023Octets = fields[17];
+        this.inPackets1024To1518Octets = fields[18];
+        this.inPackets1519ToMaxOctets = fields[19];
+        this.inPausePackets = fields[20];
+        this.inAlignmentErrors = fields[21];
+        this.inFcsErrors = fields[22];
+        this.inSymbolErrors = fields[23];
+        this.outOctets = fields[24];
+        this.outOkPackets = fields[25];
+        this.outUnicastPackets = fields[26];
+        this.outNonUnicastPackets = fields[27];
+        this.outDiscardPackets = fields[28];
+        this.outErrorPackets = fields[29];
+        this.outBroadcastPackets = fields[30];
+        this.outMulticastPackets = fields[31];
+        this.outPausePackets = fields[32];
+        this.inRate = fields[33];
+        this.outRate = fields[34];
+        this.inNoPktSec = fields[35];
+        this.outNoPktSec = fields[36];
+        this.inLossRatio = fields[37];
+        this.outLossRatio = fields[38];
+        this.date = fields[39];
+    }
+
+    @Override
+    public String toString() {
+        return "Tl1PmPort{" +
+                "tid='" + tid + '\'' +
+                ", systemType='" + systemType + '\'' +
+                ", slot='" + slot + '\'' +
+                ", port='" + port + '\'' +
+                ", time='" + time + '\'' +
+                ", inOctets='" + inOctets + '\'' +
+                ", inOkPackets='" + inOkPackets + '\'' +
+                ", inUnicastPackets='" + inUnicastPackets + '\'' +
+                ", inNonUnicastPackets='" + inNonUnicastPackets + '\'' +
+                ", inDiscardPackets='" + inDiscardPackets + '\'' +
+                ", inErrorPackets='" + inErrorPackets + '\'' +
+                ", inBroadcastPackets='" + inBroadcastPackets + '\'' +
+                ", inMulticastPackets='" + inMulticastPackets + '\'' +
+                ", inPackets64Octets='" + inPackets64Octets + '\'' +
+                ", inPackets65To127Octets='" + inPackets65To127Octets + '\'' +
+                ", inPackets128To255Octets='" + inPackets128To255Octets + '\'' +
+                ", inPackets256To511Octets='" + inPackets256To511Octets + '\'' +
+                ", inPackets512To1023Octets='" + inPackets512To1023Octets + '\'' +
+                ", inPackets1024To1518Octets='" + inPackets1024To1518Octets + '\'' +
+                ", inPackets1519ToMaxOctets='" + inPackets1519ToMaxOctets + '\'' +
+                ", inPausePackets='" + inPausePackets + '\'' +
+                ", inAlignmentErrors='" + inAlignmentErrors + '\'' +
+                ", inFcsErrors='" + inFcsErrors + '\'' +
+                ", inSymbolErrors='" + inSymbolErrors + '\'' +
+                ", outOctets='" + outOctets + '\'' +
+                ", outOkPackets='" + outOkPackets + '\'' +
+                ", outUnicastPackets='" + outUnicastPackets + '\'' +
+                ", outNonUnicastPackets='" + outNonUnicastPackets + '\'' +
+                ", outDiscardPackets='" + outDiscardPackets + '\'' +
+                ", outErrorPackets='" + outErrorPackets + '\'' +
+                ", outBroadcastPackets='" + outBroadcastPackets + '\'' +
+                ", outMulticastPackets='" + outMulticastPackets + '\'' +
+                ", outPausePackets='" + outPausePackets + '\'' +
+                ", inRate='" + inRate + '\'' +
+                ", outRate='" + outRate + '\'' +
+                ", inNoPktSec='" + inNoPktSec + '\'' +
+                ", outNoPktSec='" + outNoPktSec + '\'' +
+                ", inLossRatio='" + inLossRatio + '\'' +
+                ", outLossRatio='" + outLossRatio + '\'' +
+                ", date='" + date + '\'' +
+                '}';
     }
 }
