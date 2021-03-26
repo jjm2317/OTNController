0533286cbfbd0038a0000523ee194960c2ea109c
jjm2317 <wlaks2317@gmail.com>
2021-03-26 15:20:02 +0900

    [ADD] (cm) otnCmInventory class

--- src/main/java/com/woorinet/plugin/demo/DTO/OTN/OtnCmInventory.java
+++ src/main/java/com/woorinet/plugin/demo/DTO/OTN/OtnCmInventory.java
@@ -1,4 +1,74 @@
 package com.woorinet.plugin.demo.DTO.OTN;
 
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Setter
+@Getter
+@Data
+@Entity
 public class OtnCmInventory {
+    @Id
+    @GeneratedValue
+    int otnCmInventoryId;
+    String tid;
+    String slot;
+    String unitType;
+    String pkgVersion;
+    String pkgFilename;
+    String pkgVersionDate;
+    String pkgSize;
+    String fpgaVersion;
+    String fpgaVersionDate;
+    String fpgaSize;
+    String cpldVersion;
+    String cpldVersionDate;
+    String cpldSize;
+    String hwVersion;
+
+    public OtnCmInventory() {
+    }
+
+    public OtnCmInventory(String tid, String slot, String unitType, String pkgVersion, String pkgFilename, String pkgVersionDate, String pkgSize, String fpgaVersion, String fpgaVersionDate, String fpgaSize, String cpldVersion, String cpldVersionDate, String cpldSize, String hwVersion) {
+        this.tid = tid;
+        this.slot = slot;
+        this.unitType = unitType;
+        this.pkgVersion = pkgVersion;
+        this.pkgFilename = pkgFilename;
+        this.pkgVersionDate = pkgVersionDate;
+        this.pkgSize = pkgSize;
+        this.fpgaVersion = fpgaVersion;
+        this.fpgaVersionDate = fpgaVersionDate;
+        this.fpgaSize = fpgaSize;
+        this.cpldVersion = cpldVersion;
+        this.cpldVersionDate = cpldVersionDate;
+        this.cpldSize = cpldSize;
+        this.hwVersion = hwVersion;
+    }
+
+    @Override
+    public String toString() {
+        return "OtnCmInventory{" +
+                "tid='" + tid + '\'' +
+                ", slot='" + slot + '\'' +
+                ", unitType='" + unitType + '\'' +
+                ", pkgVersion='" + pkgVersion + '\'' +
+                ", pkgFilename='" + pkgFilename + '\'' +
+                ", pkgVersionDate='" + pkgVersionDate + '\'' +
+                ", pkgSize='" + pkgSize + '\'' +
+                ", fpgaVersion='" + fpgaVersion + '\'' +
+                ", fpgaVersionDate='" + fpgaVersionDate + '\'' +
+                ", fpgaSize='" + fpgaSize + '\'' +
+                ", cpldVersion='" + cpldVersion + '\'' +
+                ", cpldVersionDate='" + cpldVersionDate + '\'' +
+                ", cpldSize='" + cpldSize + '\'' +
+                ", hwVersion='" + hwVersion + '\'' +
+                '}';
+    }
 }
