809ef624113c96fd874b37c595a6ee3f6c3727db
jjm2317 <wlaks2317@gmail.com>
2021-03-26 14:49:43 +0900

    fix: otn node

--- src/main/java/com/woorinet/plugin/demo/Manager/OtnManager.java
+++ src/main/java/com/woorinet/plugin/demo/Manager/OtnManager.java
@@ -256,7 +256,7 @@ public class OtnManager {
                         tl1NodeState.getSOFTWARE(), // sw_ver
                         tl1NodeState.getIP_ADDR(), // ip_addr
                         tl1SystemInfo==null? "":tl1SystemInfo.getVENDOR(), // vendor
-                        tl1Inventory==null||tl1Inventory.getSERIAL_NUM().equals("---") ? "" : tl1Inventory.getSERIAL_NUM(), // serial_num
+                        tl1Inventory==null ? "" : tl1Inventory.getSERIAL_NUM(), // serial_num
                         tl1NodeState.getNODE_TYPE() // sys_type
                 );
 
