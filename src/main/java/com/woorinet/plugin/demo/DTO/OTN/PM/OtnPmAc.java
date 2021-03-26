4e8613f02bd90b943ae2c1661e6c4789499f3ed0
sinkyoungdeok <tlsrudejr123@nate.com>
2021-03-26 16:51:51 +0900

    [ADD] pm_ac

---
+++ src/main/java/com/woorinet/plugin/demo/DTO/OTN/PM/OtnPmAc.java
@@ -0,0 +1,72 @@
+package com.woorinet.plugin.demo.DTO.OTN.PM;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Data
+@Getter
+@Setter
+@Table(name = "otn_pm_ac")
+public class OtnPmAc {
+    @Id
+    @GeneratedValue
+    int pmAcId;
+    String tid;
+    String systemType;
+    String slot;
+    String port;
+    String time;
+    String name;
+    String ingPackets;
+    String ingBytes;
+    String ingRate;
+    String egrPackets;
+    String egrBytes;
+    String egrRate;
+    String date;
+
+    public OtnPmAc() {
+    }
+
+    public OtnPmAc(String tid, String systemType, String slot, String port, String time, String name, String ingPackets, String ingBytes, String ingRate, String egrPackets, String egrBytes, String egrRate, String date) {
+        this.tid = tid;
+        this.systemType = systemType;
+        this.slot = slot;
+        this.port = port;
+        this.time = time;
+        this.name = name;
+        this.ingPackets = ingPackets;
+        this.ingBytes = ingBytes;
+        this.ingRate = ingRate;
+        this.egrPackets = egrPackets;
+        this.egrBytes = egrBytes;
+        this.egrRate = egrRate;
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "OtnPmAc{" +
+                "tid='" + tid + '\'' +
+                ", systemType='" + systemType + '\'' +
+                ", slot='" + slot + '\'' +
+                ", port='" + port + '\'' +
+                ", time='" + time + '\'' +
+                ", name='" + name + '\'' +
+                ", ingPackets='" + ingPackets + '\'' +
+                ", ingBytes='" + ingBytes + '\'' +
+                ", ingRate='" + ingRate + '\'' +
+                ", egrPackets='" + egrPackets + '\'' +
+                ", egrBytes='" + egrBytes + '\'' +
+                ", egrRate='" + egrRate + '\'' +
+                ", date='" + date + '\'' +
+                '}';
+    }
+}
