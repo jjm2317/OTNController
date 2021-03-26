9b6fb23ea0458aafaeeaca1dd35dc45ca320424b
jjm2317 <wlaks2317@gmail.com>
2021-03-26 15:48:42 +0900

    [ADD] (cm) otnCmQkdLink class

--- src/main/java/com/woorinet/plugin/demo/DTO/OTN/OtnCmQkdLink.java
+++ src/main/java/com/woorinet/plugin/demo/DTO/OTN/OtnCmQkdLink.java
@@ -1,4 +1,71 @@
 package com.woorinet.plugin.demo.DTO.OTN;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+@Getter
+@Setter
+@Data
+@Entity
 public class OtnCmQkdLink {
+    @Id
+    @GeneratedValue
+    int otnCmQkdLink;
+    String tid;
+    String slot;
+    String port;
+    String server;
+    String serverPort;
+    String masterSaeId;
+    String slaveSaeId;
+    String keyIntfIp;
+    String keyIntfNetmask;
+    String keyIntfGateway;
+    String keySkeySize;
+    String kmsStatus;
+    String vendor;
+
+    public OtnCmQkdLink() {
+    }
+
+    public OtnCmQkdLink(String tid, String slot, String port, String server, String serverPort, String masterSaeId, String slaveSaeId, String keyIntfIp, String keyIntfNetmask, String keyIntfGateway, String keySkeySize, String kmsStatus, String vendor) {
+        this.tid = tid;
+        this.slot = slot;
+        this.port = port;
+        this.server = server;
+        this.serverPort = serverPort;
+        this.masterSaeId = masterSaeId;
+        this.slaveSaeId = slaveSaeId;
+        this.keyIntfIp = keyIntfIp;
+        this.keyIntfNetmask = keyIntfNetmask;
+        this.keyIntfGateway = keyIntfGateway;
+        this.keySkeySize = keySkeySize;
+        this.kmsStatus = kmsStatus;
+        this.vendor = vendor;
+    }
+
+    @Override
+    public String toString() {
+        return "OtnCmQkdLink{" +
+                "tid='" + tid + '\'' +
+                ", slot='" + slot + '\'' +
+                ", port='" + port + '\'' +
+                ", server='" + server + '\'' +
+                ", serverPort='" + serverPort + '\'' +
+                ", masterSaeId='" + masterSaeId + '\'' +
+                ", slaveSaeId='" + slaveSaeId + '\'' +
+                ", keyIntfIp='" + keyIntfIp + '\'' +
+                ", keyIntfNetmask='" + keyIntfNetmask + '\'' +
+                ", keyIntfGateway='" + keyIntfGateway + '\'' +
+                ", keySkeySize='" + keySkeySize + '\'' +
+                ", kmsStatus='" + kmsStatus + '\'' +
+                ", vendor='" + vendor + '\'' +
+                '}';
+    }
 }
