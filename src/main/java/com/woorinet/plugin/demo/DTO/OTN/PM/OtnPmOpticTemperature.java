887fd4cec64652e864660c4a66c52bec3949e9b8
sinkyoungdeok <tlsrudejr123@nate.com>
2021-03-26 17:04:07 +0900

    [ADD] pm_optic_temperature

---
+++ src/main/java/com/woorinet/plugin/demo/DTO/OTN/PM/OtnPmOpticTemperature.java
@@ -0,0 +1,57 @@
+package com.woorinet.plugin.demo.DTO.OTN.PM;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Data
+@Getter
+@Setter
+@Table(name = "otn_pm_optic_temperature")
+public class OtnPmOpticTemperature {
+    @Id
+    @GeneratedValue
+    int pmOpticTemperatureId;
+    String tid;
+    String systemType;
+    String slot;
+    String time;
+    String max;
+    String min;
+    String average;
+    String date;
+
+    public OtnPmOpticTemperature() {
+    }
+
+    @Override
+    public String toString() {
+        return "OtnPmOpticTemperature{" +
+                "tid='" + tid + '\'' +
+                ", systemType='" + systemType + '\'' +
+                ", slot='" + slot + '\'' +
+                ", time='" + time + '\'' +
+                ", max='" + max + '\'' +
+                ", min='" + min + '\'' +
+                ", average='" + average + '\'' +
+                ", date='" + date + '\'' +
+                '}';
+    }
+
+    public OtnPmOpticTemperature(String tid, String systemType, String slot, String time, String max, String min, String average, String date) {
+        this.tid = tid;
+        this.systemType = systemType;
+        this.slot = slot;
+        this.time = time;
+        this.max = max;
+        this.min = min;
+        this.average = average;
+        this.date = date;
+    }
+}
---
+++ src/main/java/com/woorinet/plugin/demo/Repository/OTN/PM/OtnPmOpticTemperatureRepository.java
@@ -0,0 +1,7 @@
+package com.woorinet.plugin.demo.Repository.OTN.PM;
+
+import com.woorinet.plugin.demo.DTO.OTN.PM.OtnPmOpticTemperature;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OtnPmOpticTemperatureRepository extends JpaRepository<OtnPmOpticTemperature, Integer> {
+}
