26091bd9e5991014c0850d576ec54b883330ed4c
sinkyoungdeok <tlsrudejr123@nate.com>
2021-03-26 16:53:54 +0900

    [ADD] pm_optic

---
+++ src/main/java/com/woorinet/plugin/demo/DTO/OTN/PM/OtnPmOptic.java
@@ -0,0 +1,69 @@
+package com.woorinet.plugin.demo.DTO.OTN.PM;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Data
+@Getter
+@Setter
+@Table(name = "otn_pm_optic")
+public class OtnPmOptic {
+    @Id
+    @GeneratedValue
+    int pmOpticId;
+    String tid;
+    String systemType;
+    String slot;
+    String port;
+    String pmType;
+    String rxMinPower;
+    String rxMaxPower;
+    String rxAvgPower;
+    String txMinPower;
+    String txMaxPower;
+    String txAvgPower;
+    String date;
+
+    public OtnPmOptic() {
+    }
+
+    public OtnPmOptic(String tid, String systemType, String slot, String port, String pmType, String rxMinPower, String rxMaxPower, String rxAvgPower, String txMinPower, String txMaxPower, String txAvgPower, String date) {
+        this.tid = tid;
+        this.systemType = systemType;
+        this.slot = slot;
+        this.port = port;
+        this.pmType = pmType;
+        this.rxMinPower = rxMinPower;
+        this.rxMaxPower = rxMaxPower;
+        this.rxAvgPower = rxAvgPower;
+        this.txMinPower = txMinPower;
+        this.txMaxPower = txMaxPower;
+        this.txAvgPower = txAvgPower;
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "OtnPmOptic{" +
+                "tid='" + tid + '\'' +
+                ", systemType='" + systemType + '\'' +
+                ", slot='" + slot + '\'' +
+                ", port='" + port + '\'' +
+                ", pmType='" + pmType + '\'' +
+                ", rxMinPower='" + rxMinPower + '\'' +
+                ", rxMaxPower='" + rxMaxPower + '\'' +
+                ", rxAvgPower='" + rxAvgPower + '\'' +
+                ", txMinPower='" + txMinPower + '\'' +
+                ", txMaxPower='" + txMaxPower + '\'' +
+                ", txAvgPower='" + txAvgPower + '\'' +
+                ", date='" + date + '\'' +
+                '}';
+    }
+}
