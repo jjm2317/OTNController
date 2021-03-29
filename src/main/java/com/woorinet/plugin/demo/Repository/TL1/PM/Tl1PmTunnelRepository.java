6806f13d6708da3a1114aa54c343501324dcb6a5
sinkyoungdeok <tlsrudejr123@nate.com>
2021-03-29 14:29:27 +0900

    [UPDATE] pm_tunnel 에 syncDate로 검색기능 추가

--- src/main/java/com/woorinet/plugin/demo/Repository/TL1/PM/Tl1PmTunnelRepository.java
+++ src/main/java/com/woorinet/plugin/demo/Repository/TL1/PM/Tl1PmTunnelRepository.java
@@ -2,5 +2,9 @@ package com.woorinet.plugin.demo.Repository.TL1.PM;
 
 import com.woorinet.plugin.demo.DTO.TL1.PM.Tl1PmTunnel;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
 public interface Tl1PmTunnelRepository extends JpaRepository<Tl1PmTunnel, Integer>{
+    List<Tl1PmTunnel> findTl1PmTunnelBySyncDate(String syncDate);
 }
