f0853302aad3050ed871d7ed9a0091f6a9ce6ce2
sinkyoungdeok <tlsrudejr123@nate.com>
2021-03-29 14:32:24 +0900

    [UPDATE] Tl1EthPort 에 syncDate로 검색기능 추가

--- src/main/java/com/woorinet/plugin/demo/Repository/TL1/Tl1EthPortRepository.java
+++ src/main/java/com/woorinet/plugin/demo/Repository/TL1/Tl1EthPortRepository.java
@@ -3,6 +3,8 @@ package com.woorinet.plugin.demo.Repository.TL1;
 import com.woorinet.plugin.demo.DTO.TL1.Tl1EthPort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface Tl1EthPortRepository extends JpaRepository<Tl1EthPort, Integer> {
+import java.util.List;
 
+public interface Tl1EthPortRepository extends JpaRepository<Tl1EthPort, Integer> {
+    List<Tl1EthPort> findTl1EthPortBySyncDate(String syncDate);
 }
