0a1e6bb9b655ab3c3059d13ebedd842301158b36
sinkyoungdeok <tlsrudejr123@nate.com>
2021-03-29 14:25:13 +0900

    [UPDATE] sess_state 에 syncDate로 검색기능 추가

--- src/main/java/com/woorinet/plugin/demo/Repository/TL1/CM/Tl1SessStateRepository.java
+++ src/main/java/com/woorinet/plugin/demo/Repository/TL1/CM/Tl1SessStateRepository.java
@@ -3,5 +3,8 @@ package com.woorinet.plugin.demo.Repository.TL1.CM;
 import com.woorinet.plugin.demo.DTO.TL1.CM.Tl1SessState;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface Tl1SessStateRepository extends JpaRepository<Tl1SessState, Integer> {
+    List<Tl1SessState> findTl1SessStateBySyncDate(String syncDate);
 }
