0f967c1da9dcfc853c9556e2f60e5a62eb13c7db
sinkyoungdeok <tlsrudejr123@nate.com>
2021-03-29 14:22:20 +0900

    [UPDATE] cm_program_info 에 syncDate로 검색기능 추가

--- src/main/java/com/woorinet/plugin/demo/Repository/TL1/CM/Tl1CmProgramInfoRepository.java
+++ src/main/java/com/woorinet/plugin/demo/Repository/TL1/CM/Tl1CmProgramInfoRepository.java
@@ -3,5 +3,8 @@ package com.woorinet.plugin.demo.Repository.TL1.CM;
 import com.woorinet.plugin.demo.DTO.TL1.CM.Tl1CmProgramInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface Tl1CmProgramInfoRepository extends JpaRepository<Tl1CmProgramInfo, String>{
+    List<Tl1CmProgramInfo> findTl1CmProgramInfoBySyncDate(String syncDate);
 }
