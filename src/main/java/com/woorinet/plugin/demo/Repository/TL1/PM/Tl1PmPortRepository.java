1939cb8f641a3ba32da808a0ae14dd360563de13
sinkyoungdeok <tlsrudejr123@nate.com>
2021-03-29 14:27:24 +0900

    [UPDATE] pm_port 에 syncDate로 검색기능 추가

--- src/main/java/com/woorinet/plugin/demo/Repository/TL1/PM/Tl1PmPortRepository.java
+++ src/main/java/com/woorinet/plugin/demo/Repository/TL1/PM/Tl1PmPortRepository.java
@@ -2,5 +2,9 @@ package com.woorinet.plugin.demo.Repository.TL1.PM;
 
 import com.woorinet.plugin.demo.DTO.TL1.PM.Tl1PmPort;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
 public interface Tl1PmPortRepository extends JpaRepository<Tl1PmPort, Integer>{
+    List<Tl1PmPort> findTl1PmPortBySyncDate(String syncDate);
 }
