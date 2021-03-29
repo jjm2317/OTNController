cfb5dc3fbba6dd4f4dfd720064022f2f8bfa66a2
sinkyoungdeok <tlsrudejr123@nate.com>
2021-03-29 14:21:42 +0900

    [UPDATE] cm_port 에 syncDate로 검색기능 추가

--- src/main/java/com/woorinet/plugin/demo/Repository/TL1/CM/Tl1CmPortRepository.java
+++ src/main/java/com/woorinet/plugin/demo/Repository/TL1/CM/Tl1CmPortRepository.java
@@ -3,5 +3,8 @@ package com.woorinet.plugin.demo.Repository.TL1.CM;
 import com.woorinet.plugin.demo.DTO.TL1.CM.Tl1CmPort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface Tl1CmPortRepository extends JpaRepository<Tl1CmPort, Integer> {
+    List<Tl1CmPort> findTl1CmPortBySyncDate(String syncDate);
 }
