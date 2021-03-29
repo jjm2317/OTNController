e4b803363011f33a26ec9c5b735c715a0937093a
sinkyoungdeok <tlsrudejr123@nate.com>
2021-03-29 14:28:21 +0900

    [UPDATE] pm 에 syncDate로 검색기능 추가

--- src/main/java/com/woorinet/plugin/demo/Repository/TL1/PM/Tl1PmRepository.java
+++ src/main/java/com/woorinet/plugin/demo/Repository/TL1/PM/Tl1PmRepository.java
@@ -3,5 +3,8 @@ package com.woorinet.plugin.demo.Repository.TL1.PM;
 import com.woorinet.plugin.demo.DTO.TL1.PM.Tl1Pm;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface Tl1PmRepository extends JpaRepository<Tl1Pm, Integer> {
+    List<Tl1Pm> findTl1PmBySyncDate(String syncDate);
 }
