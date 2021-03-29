3b34e8b8d98fb270bc3428217b5cd6c5b26e7415
sinkyoungdeok <tlsrudejr123@nate.com>
2021-03-29 14:26:22 +0900

    [UPDATE] pm_optic 에 syncDate로 검색기능 추가

--- src/main/java/com/woorinet/plugin/demo/Repository/TL1/PM/Tl1PmOpticRepository.java
+++ src/main/java/com/woorinet/plugin/demo/Repository/TL1/PM/Tl1PmOpticRepository.java
@@ -3,5 +3,8 @@ package com.woorinet.plugin.demo.Repository.TL1.PM;
 import com.woorinet.plugin.demo.DTO.TL1.PM.Tl1PmOptic;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface Tl1PmOpticRepository extends JpaRepository<Tl1PmOptic, Integer> {
+    List<Tl1PmOptic> findTl1PmOpticBySyncDate(String syncDate);
 }
