bfa3ea4e964d5a5d950098a21269ce3ae31f913d
sinkyoungdeok <tlsrudejr123@nate.com>
2021-03-29 14:30:59 +0900

    [UPDATE] access_if 에 syncDate로 검색기능 추가

--- src/main/java/com/woorinet/plugin/demo/Repository/TL1/Tl1AccessIfRepository.java
+++ src/main/java/com/woorinet/plugin/demo/Repository/TL1/Tl1AccessIfRepository.java
@@ -3,5 +3,8 @@ package com.woorinet.plugin.demo.Repository.TL1;
 import com.woorinet.plugin.demo.DTO.TL1.Tl1AccessIf;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface Tl1AccessIfRepository extends JpaRepository<Tl1AccessIf, String> {
+    List<Tl1AccessIf> findTl1AccessIfBySyncDate(String syncDate);
 }
