9de2ea2e33d9ec5d5215e529518aeb706b39a9d3
sinkyoungdeok <tlsrudejr123@nate.com>
2021-03-29 14:26:55 +0900

    [UPDATE] pm_optic_temperature 에 syncDate로 검색기능 추가

--- src/main/java/com/woorinet/plugin/demo/Repository/TL1/PM/Tl1PmOpticTemperatureRepository.java
+++ src/main/java/com/woorinet/plugin/demo/Repository/TL1/PM/Tl1PmOpticTemperatureRepository.java
@@ -3,6 +3,9 @@ package com.woorinet.plugin.demo.Repository.TL1.PM;
 import com.woorinet.plugin.demo.DTO.TL1.PM.Tl1PmOpticTemperature;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface Tl1PmOpticTemperatureRepository extends JpaRepository<Tl1PmOpticTemperature, Integer> {
+    List<Tl1PmOpticTemperature> findTl1PmOpticTemperatureBySyncDate(String syncDate);
 }
 
