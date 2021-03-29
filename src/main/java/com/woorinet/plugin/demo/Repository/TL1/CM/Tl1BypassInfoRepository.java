15b3b00bc6693a47462d25184ba2b8f5566438f2
sinkyoungdeok <tlsrudejr123@nate.com>
2021-03-29 14:19:05 +0900

    [UPDATE] bypass_info에 syncDate로 검색기능 추가

--- src/main/java/com/woorinet/plugin/demo/Repository/TL1/CM/Tl1BypassInfoRepository.java
+++ src/main/java/com/woorinet/plugin/demo/Repository/TL1/CM/Tl1BypassInfoRepository.java
@@ -3,5 +3,8 @@ package com.woorinet.plugin.demo.Repository.TL1.CM;
 import com.woorinet.plugin.demo.DTO.TL1.CM.Tl1BypassInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface Tl1BypassInfoRepository extends JpaRepository<Tl1BypassInfo, Integer> {
+    List<Tl1BypassInfo> findTl1BypassInfoBySyncDate(String syncDate);
 }
