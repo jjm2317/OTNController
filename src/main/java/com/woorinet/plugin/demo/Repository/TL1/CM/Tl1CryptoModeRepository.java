f922f890b551a36e8eccbca10be6d7f473fb4a77
sinkyoungdeok <tlsrudejr123@nate.com>
2021-03-29 14:22:52 +0900

    [UPDATE] crypto_mode 에 syncDate로 검색기능 추가

--- src/main/java/com/woorinet/plugin/demo/Repository/TL1/CM/Tl1CryptoModeRepository.java
+++ src/main/java/com/woorinet/plugin/demo/Repository/TL1/CM/Tl1CryptoModeRepository.java
@@ -3,5 +3,8 @@ package com.woorinet.plugin.demo.Repository.TL1.CM;
 import com.woorinet.plugin.demo.DTO.TL1.CM.Tl1CryptoMode;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface Tl1CryptoModeRepository extends JpaRepository<Tl1CryptoMode, Integer> {
+    List<Tl1CryptoMode> findTl1CryptoModeBySyncDate(String syncDate);
 }
