1af7983dfd2391ea102a46937baaf20621eb2f53
sinkyoungdeok <tlsrudejr123@nate.com>
2021-03-29 14:27:53 +0900

    [UPDATE] pm_pw 에 syncDate로 검색기능 추가

--- src/main/java/com/woorinet/plugin/demo/Repository/TL1/PM/Tl1PmPwRepository.java
+++ src/main/java/com/woorinet/plugin/demo/Repository/TL1/PM/Tl1PmPwRepository.java
@@ -3,5 +3,8 @@ package com.woorinet.plugin.demo.Repository.TL1.PM;
 import com.woorinet.plugin.demo.DTO.TL1.PM.Tl1PmPw;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface Tl1PmPwRepository extends JpaRepository<Tl1PmPw, Integer> {
+    List<Tl1PmPw> findTl1PmPwBySyncDate(String syncDate);
 }
