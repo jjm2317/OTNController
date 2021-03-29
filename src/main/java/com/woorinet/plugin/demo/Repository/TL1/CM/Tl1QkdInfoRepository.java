a8196f53b31653d27f944665f0c849f561da34b8
sinkyoungdeok <tlsrudejr123@nate.com>
2021-03-29 14:24:40 +0900

    [UPDATE] qkd_info 에 syncDate로 검색기능 추가

--- src/main/java/com/woorinet/plugin/demo/Repository/TL1/CM/Tl1QkdInfoRepository.java
+++ src/main/java/com/woorinet/plugin/demo/Repository/TL1/CM/Tl1QkdInfoRepository.java
@@ -3,5 +3,8 @@ package com.woorinet.plugin.demo.Repository.TL1.CM;
 import com.woorinet.plugin.demo.DTO.TL1.CM.Tl1QkdInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface Tl1QkdInfoRepository extends JpaRepository<Tl1QkdInfo, Integer> {
+    List<Tl1QkdInfo> findTl1QkdInfoBySyncDate(String syncDate);
 }
