53d10eb1b8a22407890e04bc001adbc616cd14b3
sinkyoungdeok <tlsrudejr123@nate.com>
2021-03-29 14:23:26 +0900

    [UPDATE] key_state 에 syncDate로 검색기능 추가

--- src/main/java/com/woorinet/plugin/demo/Repository/TL1/CM/Tl1KeyStateRepository.java
+++ src/main/java/com/woorinet/plugin/demo/Repository/TL1/CM/Tl1KeyStateRepository.java
@@ -3,5 +3,8 @@ package com.woorinet.plugin.demo.Repository.TL1.CM;
 import com.woorinet.plugin.demo.DTO.TL1.CM.Tl1KeyState;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface Tl1KeyStateRepository extends JpaRepository<Tl1KeyState, Integer> {
+    List<Tl1KeyState> findTl1KeyStateBySyncDate(String syncDate);
 }
