f1534425d652e238dd378ed29eee2821054e3f79
sinkyoungdeok <tlsrudejr123@nate.com>
2021-03-29 14:25:52 +0900

    [UPDATE] pm_ac 에 syncDate로 검색기능 추가

--- src/main/java/com/woorinet/plugin/demo/Repository/TL1/PM/Tl1PmAcRepository.java
+++ src/main/java/com/woorinet/plugin/demo/Repository/TL1/PM/Tl1PmAcRepository.java
@@ -2,5 +2,9 @@ package com.woorinet.plugin.demo.Repository.TL1.PM;
 
 import com.woorinet.plugin.demo.DTO.TL1.PM.Tl1PmAc;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
 public interface Tl1PmAcRepository extends JpaRepository<Tl1PmAc, Integer>{
+    List<Tl1PmAc> findTl1PmAcBySyncDate(String syncDate);
 }
