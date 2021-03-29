7d03ecaecffb2ab8a11ee1e249ad107858ef7833
sinkyoungdeok <tlsrudejr123@nate.com>
2021-03-29 14:23:57 +0900

    [UPDATE] module_info 에 syncDate로 검색기능 추가

--- src/main/java/com/woorinet/plugin/demo/Repository/TL1/CM/Tl1ModuleInfoRepository.java
+++ src/main/java/com/woorinet/plugin/demo/Repository/TL1/CM/Tl1ModuleInfoRepository.java
@@ -3,6 +3,9 @@ package com.woorinet.plugin.demo.Repository.TL1.CM;
 import com.woorinet.plugin.demo.DTO.TL1.CM.Tl1ModuleInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 
 public interface Tl1ModuleInfoRepository extends JpaRepository<Tl1ModuleInfo, Integer> {
+    List<Tl1ModuleInfo> findTl1ModuleInfoBySyncDate(String syncDate);
 }
