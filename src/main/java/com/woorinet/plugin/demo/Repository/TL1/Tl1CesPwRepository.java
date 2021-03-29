ff3e865851b478e08b7d8df18e154a8498c0fa5c
sinkyoungdeok <tlsrudejr123@nate.com>
2021-03-29 14:32:04 +0900

    [UPDATE] Tl1CesPw 에 syncDate로 검색기능 추가

--- src/main/java/com/woorinet/plugin/demo/Repository/TL1/Tl1CesPwRepository.java
+++ src/main/java/com/woorinet/plugin/demo/Repository/TL1/Tl1CesPwRepository.java
@@ -3,6 +3,9 @@ package com.woorinet.plugin.demo.Repository.TL1;
 import com.woorinet.plugin.demo.DTO.TL1.Tl1CesPw;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface Tl1CesPwRepository extends JpaRepository<Tl1CesPw, Integer> {
+    List<Tl1CesPw> findTl1CesPwBySyncDate(String syncDate);
 }
 
